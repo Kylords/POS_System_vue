cb09af4fe9f6899225e45f9a88d2b43b4867026d
Kylords <mikaylo_veloria@dlsu.edu.ph>
2024-01-15 15:26:26 +0800

    delete order

--- src/components/OrderManager.vue
+++ src/components/OrderManager.vue
@@ -115,10 +115,47 @@ const changeStatus = async (status, orderId) => {
 };
 
 
+const DELETE_ORDER_MUTATION = gql`
+  mutation deleteOrder($input: DeleteOrderInput!) {
+    deleteOrder(input: $input) {
+      errors
+      
+    }
+  }
+`;
+
+const { mutate: deleteOrder } = useMutation(DELETE_ORDER_MUTATION)
+
+async function deleteItem(orderId) {
+
+    try {
+        const result = await deleteOrder({ input: { orderId: Number(orderId) } })
+
+        if (result.data.deleteOrder.errors == 0) {
+            console.log("success")
+            refetch()
+        }
+
+        else {
+            const errorString = result.data.deleteOrder.errors.join(', ');
+
+            console.log(errorString)
+        }
+
+    }
+
+    catch (error) {
+        console.error('Login error:', error);
+    }
+
+
+}
+
+
 </script>
 
 <template>
-    <div class="row justify-content-end">
+    <div class="row justify-content-end" style="margin: auto;">
         <div class="col-md-10">
             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li v-for="(status, index) in statuses" :key="index" class="nav-item" role="presentation">
@@ -132,7 +169,7 @@ const changeStatus = async (status, orderId) => {
 
             </ul>
         </div>
-        <div  class="col">
+        <div class="col">
             <p class="continue text-end" @click="createOrderPage">Create Order <i class="bi bi-arrow-right"></i> </p>
 
         </div>
@@ -152,67 +189,73 @@ const changeStatus = async (status, orderId) => {
 
 
     <div v-if="resultAllOrdersQuery">
-        <div v-for="order in resultAllOrdersQuery.allOrders" :key="order.id">
+        <div>
+            <div v-for="order in resultAllOrdersQuery.allOrders" :key="order.id"
+                class="row justify-content-between align-items-center rounded-3 order-card mb-4" style="margin: auto;">
 
-            <div class="card rounded-3 mb-4">
-                <div v-for="product in order.products" class="card-body p-4" :key="product.id">
-                    <div class="row d-flex justify-content-between align-items-center">
-
-                        <div class="col-md-2 col-lg-2 col-xl-2">
-                            <img :src="product.image" class="img-fluid rounded-3" alt="Cotton T-shirt">
-                        </div>
-
-                        <div class="col-md-2 col-lg-2 col-xl-2">
-                            <button class="btn btn-outline-primary" v-if="variables.status == 'Pending'"
-                                @click="changeStatus('Confirmed', Number(order.id))">Confirm Order</button>
-                            <button class="btn btn-outline-success" v-else-if="variables.status == 'Confirmed'"
-                                @click="changeStatus('Completed', Number(order.id))">Complete Order</button>
-                        </div>
-
-                        <div class="col-md-2 col-lg-2 col-xl-2">
-                            <p class="lead fw-normal mb-2">{{ product.name }}</p>
-                            <p><span class="text-muted">Product Type: </span>{{ product.productType }}</p>
-                        </div>
-
-
-                        <div class="col-md-1 col-lg-1 col-xl-1">
-
-
-                            <p class="fw-normal mb-2">Quantity: </p>
-                            <p class="fw-normal mb-2">{{ order.quantity }}</p>
-
-
-                        </div>
-
-                        <div class="col-md-2 col-lg-2 col-xl-2">
-
-
-                            <p class="fw-normal mb-2">Product Price: </p>
-                            <p class="fw-normal mb-2">₱{{ product.price }}</p>
-
-
-                        </div>
-
-
-
-
-                        <div class="col-md-1 col-lg-1 col-xl-1 offset-lg-1">
-                            <p class="fw-normal mb-2">Total Price:</p>
-                            <p class="lead fw-normal mb-2">₱{{ order.totalPrice }}</p>
-                        </div>
-                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <button type="button" class="btn-close" aria-label="Close"
-                                @click.stop="submitForm(order.id)"></button>
-
-                        </div>
-                    </div>
+                <div class="col">
+                    <button class="btn btn-outline-primary" v-if="variables.status == 'Pending'"
+                        @click="changeStatus('Confirmed', Number(order.id))">Confirm Order</button>
+                    <button class="btn btn-outline-success" v-else-if="variables.status == 'Confirmed'"
+                        @click="changeStatus('Completed', Number(order.id))">Complete Order</button>
                 </div>
+
+                <div class="col-md-8">
+                    <div v-for="product in order.products" class=" p-4" :key="product.id">
+
+                        <div class="row justify-content-between col-md-12">
+
+                            <div class="col-md-3">
+                                <img :src="product.image" class="img-fluid rounded-3" alt="Cotton T-shirt">
+                            </div>
+
+
+
+                            <div class="col-md-3 col-lg-2 col-xl-2">
+                                <p class="lead fw-normal mb-2">{{ product.name }}</p>
+                                <p><span class="text-muted">Product Type: </span>{{ product.productType }}</p>
+                            </div>
+
+
+                            <div class="col-md-2 ">
+
+
+                                <p class="fw-normal mb-2">Quantity: </p>
+                                <p class="fw-normal mb-2">{{ order.quantity }}</p>
+
+
+                            </div>
+
+                            <div class="col-md-2 col-lg-2 col-xl-2">
+
+
+                                <p class="fw-normal mb-2">Product Price: </p>
+                                <p class="fw-normal mb-2">₱{{ product.price }}</p>
+
+
+                            </div>
+
+
+
+
+                            <div class="col-md-2">
+                                <p class="fw-normal mb-2">Total Price:</p>
+                                <p class="lead fw-normal mb-2">₱{{ order.totalPrice }}</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-md-2  text-end">
+                    <button type="button" class="btn-close" aria-label="Close" @click.stop="deleteItem(order.id)"></button>
+
+                </div>
+
             </div>
 
-
         </div>
-
-
 
     </div>
 </template>
@@ -227,7 +270,10 @@ const changeStatus = async (status, orderId) => {
     margin-bottom: 0;
 }
 
-
+.order-card {
+    border-radius: 3%;
+    border: 1px solid;
+}
 </style>
 
 
